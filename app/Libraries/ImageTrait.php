1778cecf612c8f803bd9f174a340f3670f4c0354
Juan Almeida <juanelojga@gmail.com>
2015-08-24 11:53:09 +0000

    Cambio el tamaño de escudos de equipos

--- app/Libraries/ImageTrait.php
+++ app/Libraries/ImageTrait.php
@@ -7,6 +7,29 @@ use Image;
 
 trait ImageTrait 
 {
+	/**
+	 * Tamaño por defecto para las imagenes
+	 * @var array
+	 */
+	protected $_imageSize = [
+		'width' => 300,
+		'height' => 200
+		];
+
+	/**
+	 * Establecer el tamaño de una imagen
+	 * @param integer $width  ancho
+	 * @param integer $height largo
+	 */
+	protected function _setImageSize($width, $height)
+	{
+		if ($width > 0 && $height > 0 && is_integer($width) && is_integer($height)) {
+			$this->_imageSize = [
+				'width' => $width,
+				'height' => $height
+				];
+		}
+	}
 
 	/**
 	 * Obtener guarda una imagen y devuelve el path hacia ella
@@ -18,7 +41,7 @@ trait ImageTrait
 	{
 		$filename = date('Y-m-d-H:i:s'). "-" .$image->getClientOriginalName();
 		$path = public_path($directorio . $filename);
-		Image::make($image->getRealPath())->resize(300, 200)->save($path);
+		Image::make($image->getRealPath())->resize($this->_imageSize['width'], $this->_imageSize['height'])->save($path);
 		return $directorio . $filename;
 	}
 
