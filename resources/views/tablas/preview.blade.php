07f59a1825800353f3d623ee8c86f4871ce5c5bb
Juan Almeida <juanelojga@gmail.com>
2015-09-08 03:47:42 +0000

    Correcciones

--- resources/views/tablas/preview.blade.php
+++ resources/views/tablas/preview.blade.php
@@ -29,14 +29,13 @@
 					<thead >
 						<tr class="row">
 							<td  class="col-xs-1 col-sm-1">&nbsp;</td>
-							<td  class="col-xs-1 col-sm-1">&nbsp;</td>
 							<td  class="col-xs-2 hidden-sm hidden-xs">Equipo</td>
 							<td  class="col-sm-2 hidden-xs hidden-md hidden-lg">Equipo</td>
 							<td  class="col-xs-2 hidden-sm hidden-md hidden-lg">EQP</td>
 							<td  class="col-xs-1 col-sm-1">PJ</td>
-							<td  class="col-xs-1 col-sm-1 hidden-xs">PG</td>
-							<td  class="col-xs-1 col-sm-1 hidden-xs">PE</td>
-							<td  class="col-xs-1 col-sm-1 hidden-xs">PP</td>
+							<td  class="col-xs-1 col-sm-1">PG</td>
+							<td  class="col-xs-1 col-sm-1">PE</td>
+							<td  class="col-xs-1 col-sm-1">PP</td>
 							<td  class="col-xs-1 col-sm-1 hidden-xs">GF</td>
 							<td  class="col-xs-1 col-sm-1 hidden-xs">GC</td>
 							<td  class="col-xs-1 col-sm-1">GD</td>
@@ -45,15 +44,14 @@
 					</thead>
 					<tbody>
 						<tr class="row" ng-repeat='equipo in tabla'>
-							<td> <% $index + 1 %> </td>
 							<td> <img class="imagenPromo" src="/<% equipo.escudo %>" style="max-width:15px;max-height:15px;"/> </td>
 							<td  class="hidden-xs hidden-sm"><% equipo.nombre %></td>
 							<td  class="hidden-xs hidden-md hidden-lg"><% equipo.nombre_corto %></td>
 							<td  class="hidden-sm hidden-md hidden-lg"><% equipo.abreviatura %></td>
 							<td> <% equipo.partidos_jugados %> </td>
-							<td  class="hidden-xs"> <% equipo.partidos_ganados %> </td>
-							<td  class="hidden-xs"> <% equipo.partidos_empatados %> </td>
-							<td  class="hidden-xs"> <% equipo.partidos_perdidos %> </td>
+							<td> <% equipo.partidos_ganados %> </td>
+							<td> <% equipo.partidos_empatados %> </td>
+							<td> <% equipo.partidos_perdidos %> </td>
 							<td class="hidden-xs"> <% equipo.goles_favor %> </td>
 							<td class="hidden-xs"> <% equipo.goles_contra %> </td>
 							<td> <% equipo.goles_diferencia %> </td>
