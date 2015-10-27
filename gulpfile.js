65ee8e22be30e9f2ce5885b2e0caf2d3b7e52620
Juan Almeida <juanelojga@gmail.com>
2015-10-26 21:25:52 -0500

    Configuracion de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
+var gulp = require('gulp');
+
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+gulp.task("copyfiles", function() {
 
-elixir(function(mix) {
-    mix.phpUnit();
+	gulp.src("public/js/libs/jquery/dist/jquery.min.js")
+		.pipe(gulp.dest("resources/assets/js/"));
+
+	gulp.src("public/js/libs/bootstrap/less/**")
+		.pipe(gulp.dest("resources/assets/less/bootstrap"));
+
+	gulp.src("public/js/libs/bootstrap/dist/js/bootstrap.min.js")
+		.pipe(gulp.dest("resources/assets/js/"));
+
+	gulp.src("public/js/libs/bootstrap/dist/fonts/**")
+	.pipe(gulp.dest("public/assets/fonts"));
+
 });
+/**
+* Default gulp is to run this elixir stuff
+*/
+elixir(function(mix) {
+	// Combine scripts
+	mix.scripts([
+		'js/jquery.js',
+		'js/bootstrap.js'
+	],
+		'public/assets/js/admin.js',
+		'resources/assets'
+	);
+	// Compile Less
+	mix.less('admin.less', 'public/assets/css/admin.css');
+})
