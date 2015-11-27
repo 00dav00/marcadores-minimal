4d3dcd14c9a3b5a3ee31c6676bf05d8786fe52d4
David Revelo <david@aperturaradical.org>
2015-11-26 22:53:25 -0500

    Agregar conexion a api rest para manejo de jugadores titulares

--- public/js/services.js
+++ public/js/services.js
@@ -202,3 +202,23 @@ torneoServices.factory('Tablas',
 		}
 	]
 );
+
+torneoServices.factory('Titulares',
+	[
+		'$resource',
+		function($resource)	{
+			return	$resource(
+				"/api/partido/:partido/titulares",
+				{partido: '@partido_id'},
+				{
+					bulk: 	{method:	'POST',	cache:	false,	isArray:	true },
+					query: 	{method:	'GET',	cache:	false,	isArray:	true },
+					// get:	{method:'GET',	cache:false,	isArray:false},
+					// save:	{method:'POST',	cache:false,	isArray:false},
+					// update:	{method:'PUT',	cache:false,	isArray:false},
+					// delete:	{method:'DELETE',	cache:false,	isArray:false},
+				}
+			);
+		}
+	]
+);
